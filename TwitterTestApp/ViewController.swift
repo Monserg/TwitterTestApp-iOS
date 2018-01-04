389accd87e1aef73a0aa33c3043b03c11257c55e
SerhiiM <msm72@mail.ru>
2018-01-04 13:25:40 +0200

    first commit

---
+++ TwitterTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterTestApp
+//
+//  Created by msm72 on 04.01.2018.
+//  Copyright © 2018 msm72. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
